ac4848e994793e60ab206374ed0d9ebabb753876
Ihor Didyk <ihordidyk95@gmail.com>
2019-05-18 20:23:12 +0300

    configured web security with jwt

---
+++ src/main/java/io/pyxis/api/config/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package io.pyxis.api.config;
+
+import io.pyxis.api.security.JwtFilterConfigurerAdapter;
+import io.pyxis.api.security.JwtTokenProvider;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@AllArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${api.v1.endpoint}")
+    private String mainEndpoint;
+
+    private final JwtTokenProvider tokenProvider;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder(12);
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .httpBasic().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeRequests()
+                .antMatchers(mainEndpoint + "/").permitAll()
+                .antMatchers(mainEndpoint + "admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .apply(new JwtFilterConfigurerAdapter(tokenProvider));
+    }
+}
